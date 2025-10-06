4010e7b08cc4457b34656297669dbd10ffc85e28
Mithun P <59569203+Mithunitp@users.noreply.github.com>
2025-10-06 15:52:44 +0530

    Create FoundryOptions.cs

---
+++ PowerPointAI/Models/FoundryOptions.cs
@@ -0,0 +1,9 @@
+namespace PowerpointAi.Models
+{
+    public class FoundryOptions
+    {
+        public string Endpoint { get; set; } = string.Empty;
+        public string ApiKey { get; set; } = string.Empty;
+        public string ProjectId { get; set; } = string.Empty;
+    }
+}
