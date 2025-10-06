051c0ecbe557be981f6d5392502b1371f73ee5d4
Mithun P <59569203+Mithunitp@users.noreply.github.com>
2025-10-06 15:51:23 +0530

    Create McpMessage.cs

---
+++ PowerPointAI/Models/McpMessage.cs
@@ -0,0 +1,8 @@
+namespace PowerpointAi.Models
+{
+    public class McpMessage
+    {
+        public string Agent { get; set; } = string.Empty;
+        public string Input { get; set; } = string.Empty;
+    }
+}
