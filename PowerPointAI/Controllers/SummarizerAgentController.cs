0a53f1ccdae79372d31f5b79c4223072dd9519d3
Mithun P <59569203+Mithunitp@users.noreply.github.com>
2025-10-06 17:14:11 +0530

    Update SummarizerAgentController.cs

--- PowerPointAI/Controllers/SummarizerAgentController.cs
+++ PowerPointAI/Controllers/SummarizerAgentController.cs
@@ -1,1 +1,26 @@
 
+using Microsoft.AspNetCore.Mvc;
+using PowerpointAi.Models;
+using PowerpointAi.Services;
+
+namespace PowerpointAi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SummarizerAgentController : ControllerBase
+    {
+        private readonly OrchestratorService _orchestrator;
+
+        public SummarizerAgentController(OrchestratorService orchestrator)
+        {
+            _orchestrator = orchestrator;
+        }
+
+        [HttpPost("run")]
+        public async Task<IActionResult> RunSummarizer([FromBody] PresentationRequest request)
+        {
+            var output = await _orchestrator.RunSummarizerAsync(request.Topic);
+            return Ok(new { SummaryOutput = output });
+        }
+    }
+}
